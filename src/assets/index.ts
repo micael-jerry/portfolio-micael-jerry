cbf40fe7ef1f781e4983d175c69e0032d24c8376
Micael Jerry <hei.jerry.2@gmail.com>
2024-06-12 12:13:41 +0300

    fix: formatting contact page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,4 +3,8 @@ export const IMAGES = {
 	blueGradientBg: new URL("./images/blue-gradient-bg.png", import.meta.url)
 		.href,
 	jerryImg: new URL("./images/jerry.jpg", import.meta.url).href,
+	contactImageIllustrator: new URL(
+		"./images/contact-removebg.png",
+		import.meta.url,
+	).href,
 };
--- src/sections/contact/ContactSection.tsx
+++ src/sections/contact/ContactSection.tsx
@@ -1,19 +1,60 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, Button, Card, Grid, Typography } from "@mui/material";
+import { IMAGES } from "../../assets";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SectionIdEnum } from "../../types/sectionId/sectionId";
+import "./ContactSection.css"
 
 export const ContactSection: React.FC = () => {
 	return (
-		<Box py={4}>
-			<Typography variant="h6">Hello, my name is</Typography>
-			<Typography variant="h2" mb={1} fontWeight={500}>
-				Micael Jerry
-			</Typography>
-			<Typography variant="h5" mb={3}>
-				Computer science student with a passion for technology
-			</Typography>
-			<Typography maxWidth={500}>
-				I am passionate about technology and I love to learn new things.
-			</Typography>
+		<Box
+			display={"flex"}
+			alignItems={"center"}
+			pt={"6%"}
+			pb={4}
+			height={"80vh"}
+		>
+			<Grid container spacing={4} alignItems="center">
+				<Grid item xs={12} md={6}>
+					<Box>
+						<Typography variant="h6">Hello, my name is</Typography>
+						<Typography variant="h2" mb={1} fontWeight={500}>
+							Micael Jerry
+						</Typography>
+						<Typography variant="h5" mb={3}>
+							Computer science student with a passion for technology
+						</Typography>
+						<Typography maxWidth={500}>
+							I am passionate about technology and I love to learn new things.
+						</Typography>
+					</Box>
+					<Box sx={{ display: "flex", gap: 2, marginTop: 2 }}>
+						<AnchorLink href={`#${SectionIdEnum.ABOUT}`}>
+							<Button variant="contained" color="warning" size="large">
+								About
+							</Button>
+						</AnchorLink>
+						<AnchorLink href={`#${SectionIdEnum.CONTACT}`}>
+							<Button variant="outlined" color="warning" size="large">
+								Contact
+							</Button>
+						</AnchorLink>
+					</Box>
+				</Grid>
+				<Grid item xs={12} md={6}>
+					<Card
+						sx={{
+							boxShadow: 0,
+							backgroundColor: "transparent"
+						}}
+					>
+						<img
+							alt="contact illustrator"
+							src={IMAGES.contactImageIllustrator}
+							id="contact-img-illustrator"
+						/>
+					</Card>
+				</Grid>
+			</Grid>
 		</Box>
 	);
 };
