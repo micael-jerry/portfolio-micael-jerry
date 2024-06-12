3a3e274940a508d25c54edf6cdb40d4ac1befed8
Micael Jerry <hei.jerry.2@gmail.com>
2024-06-12 15:00:47 +0300

    fix: remove cp

--- src/sections/about/AboutSection.tsx
+++ src/sections/about/AboutSection.tsx
@@ -1,19 +1,7 @@
-import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export const AboutSection: React.FC = () => {
 	return (
-		<Box py={4}>
-			<Typography variant="h6">Hello, my name is</Typography>
-			<Typography variant="h2" mb={1} fontWeight={500}>
-				Micael Jerry
-			</Typography>
-			<Typography variant="h5" mb={3}>
-				Computer science student with a passion for technology
-			</Typography>
-			<Typography maxWidth={500}>
-				I am passionate about technology and I love to learn new things.
-			</Typography>
-		</Box>
+		<h1>About</h1>
 	);
 };
