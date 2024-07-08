7d83f60ccf1e7109c4192d5cf8471fd8ed00928f
Micael Jerry <hei.jerry.2@gmail.com>
2024-07-08 22:11:40 +0300

    fix: nav menu animation

--- src/components/Layout/NavMenu/NavButton.tsx
+++ src/components/Layout/NavMenu/NavButton.tsx
@@ -1,4 +1,5 @@
 import { Button, ButtonProps } from "@mui/material";
+import { motion } from "framer-motion";
 import React from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
@@ -27,12 +28,28 @@ export const NavButton: React.FC<NavButtonProps> = ({ to, text, isSmall, isAncho
 
 	return !isAnchorLink ? (
 		<AnchorLink href={`#${to}`} key={to} offset={isSmall ? "56px" : "64px"} className="anchor-link">
-			<Button size="medium" fullWidth={isSmall} {...otherProps}>
+			<Button
+				component={motion.button}
+				size="medium"
+				fullWidth={isSmall}
+				{...otherProps}
+				whileHover={{ scale: 1.15 }}
+				whileTap={{ scale: 0.95 }}
+			>
 				{text}
 			</Button>
 		</AnchorLink>
 	) : (
-		<Button component="a" href={to} target="_blank" size="medium" fullWidth={isSmall} {...otherProps}>
+		<Button
+			component={motion.a}
+			href={to}
+			target="_blank"
+			size="medium"
+			fullWidth={isSmall}
+			{...otherProps}
+			whileHover={{ scale: 1.15 }}
+			whileTap={{ scale: 0.95 }}
+		>
 			{text}
 		</Button>
 	);
