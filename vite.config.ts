0123b24c7d5f61199e534ba9c7bbf8993c55e224
Micael Jerry FIDIMALALA <fidimalala.mj@gmail.com>
2026-07-01 19:36:07 +0300

    fix: ready for another deployement

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/portfolio-micael-jerry/",
+	base: "/",
 	build: {
 		rollupOptions: {
 			output: {
