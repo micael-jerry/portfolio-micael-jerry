1166ca9d1635c6dae066ac34d53931f8f8dc4997
Micael Jerry FIDIMALALA <fidimalala.mj@gmail.com>
2026-07-01 20:48:47 +0300

    fix: update seo

--- src/components/SectionContainer/SectionContainer.tsx
+++ src/components/SectionContainer/SectionContainer.tsx
@@ -9,12 +9,12 @@ export type SectionContainerProps = {
 
 export const SectionContainer: React.FC<SectionContainerProps> = ({ children, sectionId }) => {
 	return (
-		<div id={sectionId} key={sectionId}>
+		<section id={sectionId} key={sectionId}>
 			<Container maxWidth={"lg"}>
 				<Box minHeight={"100vh"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
 					{children}
 				</Box>
 			</Container>
-		</div>
+		</section>
 	);
 };
